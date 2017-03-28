d60974fc4cd877f80283d1fbafd05b2ee9e6c059
Ege  Burak Özpınar <ege_ozpinar@hotmail.com>
2017-03-28 18:40:09 +0300

    Update nonlineer-3.py

--- nonlineer-3.py
+++ nonlineer-3.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+#-- coding: utf-8 --
+
 def f(x,y):
-    return(x**2+x*y-10)
+    return(x*2+x*y-10)
+
 def g(x,y):
-    return(y+3*x*y**2-57)
+    return(y+3*x*y*2-57)
+
+def fx(x,y):
+    return(2*x+y)
+
+def fy(x,y):
+    return(x)
+
+def gx(x,y):
+    return(3*y**2)
+
+def gy(x,y):
+    return(1+6*x*y)
 
 xi = float(input("x için başlangıç değerini girin: "))
+
 yi = float(input("y için başlangıç değerini girin: "))
 
+for i in range(10000):
 
+    xi -= ((gy(xi,yi)*f(xi,yi))-(g(xi,yi)*fy(xi,yi)))/((gy(xi,yi)*fx(xi,yi))-(gx(xi,yi)*fy(xi,yi)))
+
+    yi -= ((g(xi,yi)*fx(xi,yi))-(f(xi,yi)*gx(xi,yi)))/((fx(xi,yi)*gy(xi,yi))-(fy(xi,yi)*gx(xi,yi)))
 
 print(xi,yi)
